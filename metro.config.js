3dd222e386a602ee0fd96a87c041ab1243cc7902
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2025-03-08 09:17:59 -0500

    Add initial project setup with configuration files and resources

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,6 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname)
-defaultConfig.resolver.sourceExts.push('cjs');
+config.resolver.sourceExts.push('cjs');
 
 module.exports = withNativeWind(config, { input: './global.css' })
